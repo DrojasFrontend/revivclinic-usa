7aeedb863a3fa12f96ed968a7a681d98a42486de
Daniel Rojas <danielrojas243@gmail.com>
2023-11-25 02:08:40 -0500

    fix: performarce

--- revivclinica-usa/footer.php
+++ revivclinica-usa/footer.php
@@ -1,8 +1,8 @@
   <footer id="colophon" class="site-footer" role="contentinfo">
-    FOOTER
   </footer>
 </main>
  
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css"/>
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
   <script src="<?= URL_BASE ?>/assets/js/main.js"></script>
