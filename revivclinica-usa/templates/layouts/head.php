5263729b707fcca91f1f09f93f231f3847f1da5d
Daniel Rojas <danielrojas243@gmail.com>
2023-11-25 01:24:52 -0500

    fix: performarce css, js and img

--- revivclinica-usa/templates/layouts/head.php
+++ revivclinica-usa/templates/layouts/head.php
@@ -3,7 +3,19 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap">
 </noscript>
 
-<link async rel="stylesheet" href="<?= URL_BASE ?>/assets/css/main.css">
-<link async rel="stylesheet" href="<?= URL_BASE ?>/assets/css/home.css">
+<link rel="preload" href="<?= URL_BASE ?>/assets/css/main.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link rel="stylesheet" href="<?= URL_BASE ?>/assets/css/main.css">
+</noscript>
+<link rel="preload" href="<?= URL_BASE ?>/assets/css/home.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link rel="stylesheet" href="<?= URL_BASE ?>/assets/css/home.css">
+</noscript>
 
-<link async rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<link rel="preload" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+</noscript>
+
+<link rel="preload" fetchpriority="high" as="image" href="/wp-content/uploads/2023/11/unlocking-the-healing-potential.webp" type="image/webp">
+<link rel="preload" fetchpriority="high" as="image" href="/wp-content/uploads/2023/11/unlocking-the-healing-potential-mobile.webp" type="image/webp">
