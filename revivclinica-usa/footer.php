98f6138281b1852483cb3707ecff3d67e53b1942
Daniel Rojas <danielrojas243@gmail.com>
2023-11-26 18:29:49 -0500

    refactor: fix video

--- revivclinica-usa/footer.php
+++ revivclinica-usa/footer.php
@@ -7,9 +7,6 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" defer></script>
 <script src="wp-content/themes/revivclinica-usa/assets/js/main.js" async></script>
 
-<script src="https://fast.wistia.com/embed/medias/axkmbeh150.jsonp" defer></script>
-<script src="https://fast.wistia.com/assets/external/E-v1.js" defer></script>
-
   <?php wp_footer(); ?>
 </body>
 </html>
