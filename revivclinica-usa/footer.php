496600357bd1aab89b2becf2b0569283535a7742
Daniel Rojas <danielrojas243@gmail.com>
2023-11-28 23:00:05 -0500

    fix: remove async js and css

--- revivclinica-usa/footer.php
+++ revivclinica-usa/footer.php
@@ -7,7 +7,7 @@
 <link rel="preload" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" as="script">
 <link rel="preload" href="/wp-content/themes/revivclinica-usa/assets/js/main.js" as="script">
 
-<script src="/wp-content/themes/revivclinica-usa/assets/js/jquery.min.js" async></script>
+<script src="/wp-content/themes/revivclinica-usa/assets/js/jquery.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css" defer/>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" defer></script>
 <script src="/wp-content/themes/revivclinica-usa/assets/js/main.js" async></script>
